7d95a7d399e79ea23c39974f27538fd1cd5758bc
Vysotskaya Liya <vysotskaya-liya@yandex.ru>
2024-07-25 16:24:32 +0300

    fix: update configs

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,28 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import sveltePreprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [
+		vitePreprocess(),
+		sveltePreprocess({
+			scss: {
+				includePaths: ['src/styles']
+			}
+		})
+	],
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		alias: {
+			$api: 'src/api',
+			$assets: 'src/assets',
+			$constants: 'src/constants',
+			$types: 'src/types',
+			$utils: 'src/utils',
+			$ui: 'src/ui'
+		}
 	}
 };
 
