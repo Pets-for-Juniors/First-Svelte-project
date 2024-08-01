a58bf24f44e3412a215102ea8b09ada14a11a8a0
Vysotskaya Liya <vysotskaya-liya@yandex.ru>
2024-08-01 15:04:09 +0300

    feat: add a logo zoom animation on hover

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,25 +1,36 @@
 <script lang="ts">
-  import logo from '$lib/assets/Header_animation_logo.svg';
-  import MainButton from './_ui/MainButton.svelte';
+	import logo from '$lib/assets/Header_animation_logo.svg';
+	import MainButton from './_ui/MainButton.svelte';
 
-  function handleButtonClick() {
-    alert('Button clicked!');
-  }
+	function handleButtonClick() {
+		alert('Button clicked!');
+	}
 </script>
 
-
 <header class="header">
-  <img src={logo} alt="pets logo" />
-  <MainButton text="Помогаю!" onClick={handleButtonClick}/>
+	<img class="logo" src={logo} alt="pets logo" />
+	<MainButton text="Помогаю!" onClick={handleButtonClick} />
 </header>
 
-
 <style lang="scss">
-  .header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 20px;
-    background-color: inherit;
-  }
+	.header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: 20px;
+		background-color: inherit;
+	}
+
+	.logo {
+		width: 127px;
+		height: 75px;
+		transition:
+			width 0.3s ease,
+			height 0.3s ease;
+	}
+
+	.logo:hover {
+		width: 145px;
+		height: 83.65px;
+	}
 </style>
