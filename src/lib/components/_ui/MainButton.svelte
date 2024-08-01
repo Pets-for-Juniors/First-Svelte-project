105f70fd998f5de281905292e2b1aaf781e29439
Vysotskaya Liya <vysotskaya-liya@yandex.ru>
2024-08-01 14:56:28 +0300

    feat: add an indentation effect for the button and sequential bouncing of letters

--- src/lib/components/_ui/MainButton.svelte
+++ src/lib/components/_ui/MainButton.svelte
@@ -15,36 +15,66 @@
 </script>
 
 <button class="button" on:click={handleClick}>
-	{text}
+	<div class="text-container">
+		{#each text.split('') as letter, index}
+			<span class="letter" style="animation-delay: {index * 0.1}s">{letter}</span>
+		{/each}
+	</div>
 </button>
 
 <style lang="scss">
 	.button {
 		width: fit-content;
 		padding: 22px 44px;
-
 		font-family: RubikMonoOne-Regular, sans-serif;
 		font-size: var(--medium-text);
-		letter-spacing: calc(var(--medium-text) * 0.08);
-		color: var(--white);
 		text-shadow: 0 4px 4px var(--shadow);
-
+		color: var(--white);
 		background-color: var(--green);
 		border: none;
 		border-radius: 15px;
 		cursor: pointer;
-		transition: background-color 0.3s ease;
-
-		&:hover {
-			background-color: #0056b3;
-		}
+		overflow: hidden;
+		position: relative;
+		transition: transform 0.1s ease;
 
 		&:active {
-			background-color: #004085;
+			transform: translateY(10px);
 		}
 
 		&:focus {
 			outline: none;
+		}
+	}
+
+	.text-container {
+		display: flex;
+		flex-wrap: nowrap;
+	}
+
+	.letter {
+		display: inline-block;
+		margin-right: calc(var(--medium-text) * 0.08);
+		transition: transform 0.3s ease;
+	}
+
+	.button:hover .letter {
+		animation: bounce 0.6s ease-in-out;
+	}
+
+	@keyframes bounce {
+		0%,
+		20%,
+		50%,
+		80%,
+		100% {
+			transform: translateY(0);
+		}
+		40% {
+			transform: translateY(-10px);
+		}
+		60% {
+			transform: translateY(-5px);
 		}
 	}
 </style>
