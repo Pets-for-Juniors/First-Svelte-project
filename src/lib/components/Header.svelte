9bff45d7f93ce9e07e72920f0d88c342757714d8
Vysotskaya Liya <vysotskaya-liya@yandex.ru>
2024-08-01 14:32:52 +0300

    feat: header proto and some global styles

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import logo from '$lib/assets/Header_animation_logo.svg';
+  import MainButton from './_ui/MainButton.svelte';
+
+  function handleButtonClick() {
+    alert('Button clicked!');
+  }
+</script>
+
+
+<header class="header">
+  <img src={logo} alt="pets logo" />
+  <MainButton text="Помогаю!" onClick={handleButtonClick}/>
+</header>
+
+
+<style lang="scss">
+  .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 20px;
+    background-color: inherit;
+  }
+</style>
