1cfb0cf0f082fe682f7e380f13256c40b7cd2199
Vysotskaya Liya <vysotskaya-liya@yandex.ru>
2024-08-02 12:34:31 +0300

    feat: creating the Main component as a semantic wrapper

---
+++ src/lib/components/Main.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+</script>
+
+<main class="main">
+	<slot />
+</main>
+
+<style lang="scss">
+	.main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+
+		width: 100%;
+		margin: 0 auto;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
 	import Header from '../lib/components/Header.svelte';
+	import Main from '../lib/components/Main.svelte';
 </script>
 
 <Header />
+
+<Main />
