76742a4b8220a3aee8785463838a00b339153958
Vysotskaya Liya <vysotskaya-liya@yandex.ru>
2024-08-02 16:24:40 +0300

    feat: create overlay proto

---
+++ src/lib/components/Overlay.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+	import { onDestroy, onMount } from 'svelte';
+
+	export let isOpen: boolean;
+	export let onClose: () => void;
+
+	const handleEscClose = (e: KeyboardEvent) => {
+		if (e.key === 'Escape') {
+			onClose();
+		}
+	};
+
+	onMount(() => {
+		document.addEventListener('keydown', handleEscClose);
+
+		onDestroy(() => {
+			document.removeEventListener('keydown', handleEscClose);
+		});
+	});
+</script>
+
+<div
+	class={`overlay ${isOpen ? 'overlayOpen' : ''}`}
+	on:click={(e) => {
+		if (e.currentTarget === e.target) {
+			onClose();
+		}
+	}}
+>
+	<slot></slot>
+</div>
+
+<style lang="scss">
+	.overlay {
+		position: fixed;
+		z-index: 11;
+		top: 0;
+		left: 0;
+
+		overflow: auto;
+		display: flex;
+		justify-content: center;
+
+		box-sizing: border-box;
+		width: 100%;
+		height: 100vh;
+		padding: 0;
+
+		visibility: hidden;
+		background-color: var(--shadow);
+
+		&::-webkit-scrollbar {
+			display: none;
+		}
+
+		&.overlayOpen {
+			visibility: visible;
+		}
+	}
+</style>
