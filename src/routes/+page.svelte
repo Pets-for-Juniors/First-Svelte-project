42951d6d74b2a190f5c0f25fe169f166dc0bac3b
Vysotskaya Liya <vysotskaya-liya@yandex.ru>
2024-08-02 16:56:06 +0300

    feat: create PopupHelp proto

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
-	import Header from '../lib/components/Header.svelte';
-	import Main from '../lib/components/Main.svelte';
-	import Promo from '../lib/components/Promo.svelte';
+	import Header from '$lib/components/Header.svelte';
+	import Main from '$lib/components/Main.svelte';
+	import PopupHelp from '$lib/components/PopupHelp.svelte';
+	import Promo from '$lib/components/Promo.svelte';
 </script>
 
 <Header />
 
 <Main>
 	<Promo />
+
+	<PopupHelp isOpen={true} onClose={() => {}} />
 </Main>
