0268717c7d1f002481e1c4aee8186f7478700031
Liya Vysotskaya <Imredrat@yandex.ru>
2024-10-21 18:13:40 +0300

    feat: functionality proto

--- src/lib/components/AboutTeamSection.svelte
+++ src/lib/components/AboutTeamSection.svelte
@@ -1,98 +1,51 @@
 <script lang="ts">
+	import { EmployeeDataSource } from '$lib/dataSources/EmployeeDataSource';
+	import { onMount } from 'svelte';
+	import EmployeeCard from './EmployeeCard.svelte';
+
+	let employeeDataSource: EmployeeDataSource;
+
+	onMount(async () => {
+		const ds = new EmployeeDataSource();
+		await ds.load();
+		employeeDataSource = ds;
+	});
+
+	// async function onPrevEmployee() {
+	// 	employeeDataSource = await employeeDataSource.loadPrev();
+	// }
+
+	// async function onNextEmployee() {
+	// 	employeeDataSource = await employeeDataSource.loadNext();
+	// }
+
+	$: loaded = employeeDataSource?.loaded;
 </script>
 
 <section class="team">
-	<div class="content">
-		<div class="textWrapper">
-			<h2 class="title">Графический дизайнер, иллюстратор</h2>
-			<p class="description">
-				Ololo is a team of skilled and passionate developers with a focus on creating innovative and
-				user-friendly solutions.
-			</p>
-
-			<p class="name">Ololoshechka</p>
-		</div>
-
-		<img class="image" src="#" alt="Ololoshechka" />
-	</div>
+	{#if loaded}
+		<!-- <EmployeeCard {employeeDataSource} {onPrevEmployee} {onNextEmployee} /> -->
+		<EmployeeCard {employeeDataSource} />
+	{:else}
+		<p class="text">Загрузка...</p>
+	{/if}
 </section>
 
 <style lang="scss">
 	.team {
 		width: 100%;
 		max-width: var(--section-max-width);
+		height: 100%;
+		min-height: 1000px;
 
 		padding: 75px 0 90px;
 		display: flex;
 	}
 
-	.content {
-		width: 100%;
-		min-height: 740px;
-		padding: 45px 80px;
-
-		position: relative;
-		display: flex;
-
-		border: 5px solid var(--dark-blue);
-		border-radius: var(--border-radius-section);
-
-		text-align: center;
-		color: var(--dark-blue);
-		font-size: 24px;
-
-		background: var(--white);
-
-		gap: 80px;
-	}
-
-	.textWrapper {
-		margin-top: 30px;
-	}
-
-	.title {
-		width: 100%;
-		max-width: 442px;
-
-		font-family: Inter-Bold, sans-serif;
+	.text {
+		font-family: Inter-Regular, sans-serif;
 		font-size: var(--large-text);
-		text-align: left;
 		letter-spacing: var(--large-text-letter-spacing);
 		color: var(--red);
-	}
-
-	.description {
-		margin-top: 45px;
-
-		width: 100%;
-		max-width: 683px;
-
-		font-family: Inter-Regular, sans-serif;
-		font-size: var(--small-text);
-		letter-spacing: var(--small-text-letter-spacing);
-		text-align: left;
-	}
-
-	.name {
-		margin-top: 150px;
-
-		font-family: RubikMonoOne-Regular, sans-serif;
-		font-size: var(--extra-large-text);
-		letter-spacing: var(--extra-large-text-letter-spacing);
-		text-transform: uppercase;
-		text-decoration: none;
-		text-align: left;
-	}
-
-	.image {
-		width: 100%;
-		max-width: 470px;
-		height: 100%;
-		max-height: 650px;
-
-		object-fit: cover;
-
-		border: 10px solid var(--amber);
-		border-radius: var(--border-radius-section);
 	}
 </style>
---
+++ src/lib/components/EmployeeCard.svelte
@@ -0,0 +1,98 @@
+<script lang="ts">
+	import type { EmployeeDataSource } from '$lib/dataSources/EmployeeDataSource';
+
+	export let employeeDataSource: EmployeeDataSource;
+	// export let onPrevEmployee: () => void = () => {};
+	// export let onNextEmployee: () => void = () => {};
+
+	// const onNext = () => {
+	// 	onNextEmployee();
+	// };
+
+	// const onPrev = () => {
+	// 	onPrevEmployee();
+	// };
+</script>
+
+<div class="content">
+	<div class="textWrapper">
+		<h2 class="title">{employeeDataSource.results[0].job_title}</h2>
+		<p class="description">
+			{employeeDataSource.results[0].about_person}
+		</p>
+
+		<p class="name">{employeeDataSource.results[0].name}</p>
+	</div>
+
+	<img class="image" src={employeeDataSource.results[0].photo} alt="Ololoshechka" />
+</div>
+
+<style lang="scss">
+	.content {
+		width: 100%;
+		max-height: 740px;
+		padding: 45px 80px;
+
+		position: relative;
+		display: flex;
+
+		border: 5px solid var(--dark-blue);
+		border-radius: var(--border-radius-section);
+
+		text-align: center;
+		color: var(--dark-blue);
+		font-size: 24px;
+
+		background: var(--white);
+
+		gap: 80px;
+	}
+
+	.textWrapper {
+		margin-top: 30px;
+	}
+
+	.title {
+		width: 100%;
+		max-width: 442px;
+
+		font-family: Inter-Bold, sans-serif;
+		font-size: var(--large-text);
+		text-align: left;
+		letter-spacing: var(--large-text-letter-spacing);
+		color: var(--red);
+	}
+
+	.description {
+		margin-top: 45px;
+
+		width: 100%;
+		max-width: 683px;
+
+		font-family: Inter-Regular, sans-serif;
+		font-size: var(--small-text);
+		letter-spacing: var(--small-text-letter-spacing);
+		text-align: left;
+	}
+
+	.name {
+		margin-top: 150px;
+
+		font-family: RubikMonoOne-Regular, sans-serif;
+		font-size: var(--extra-large-text);
+		letter-spacing: var(--extra-large-text-letter-spacing);
+		text-transform: uppercase;
+		text-decoration: none;
+		text-align: left;
+	}
+
+	.image {
+		width: 470px;
+		height: 650px;
+
+		object-fit: cover;
+
+		border: 10px solid var(--amber);
+		border-radius: var(--border-radius-section);
+	}
+</style>
